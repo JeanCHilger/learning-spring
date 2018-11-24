e6e07eec63d82ea9414b433debb9d55e495c143d
JeanCHilger <hilgerjeancarlo@gmail.com>
2018-11-24 04:30:03 -0200

    Basic Spring Data JPA project

--- hello-world/src/main/java/com/jeanhilger/hello/HelloWorldApplication.java
+++ hello-world/src/main/java/com/jeanhilger/hello/HelloWorldApplication.java
@@ -3,10 +3,13 @@ package com.jeanhilger.hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//The @SpringBootApplication adds the annotations:
+//@Configuration, @EnableAutoConfiguration, @EnableWebMvc, @ComponentScan
 @SpringBootApplication
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
+		// Launches the application
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
 }
